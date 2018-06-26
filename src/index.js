e0d177d62d10e77562dd792051a952596de943cf
Dmitriy Pavlenko <azeos1101@ya.ru>
2018-06-26 15:23:29 +0300

    update package version

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import program from 'commander';
 
 const gendiff = () => {
   program
-    .version('0.0.1')
+    .version('0.1.0')
     .arguments('<firstConfig> <secondConfig>')
     .description('Compares two configuration files and shows a difference.')
     .option('-f, --format [type]', 'Output format')
