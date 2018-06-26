d3be644c3c8c8455659b1d2ea7aea1026ff62802
Dmitriy Pavlenko <azeos1101@ya.ru>
2018-06-26 17:13:36 +0300

    update package version to 0.2.0

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -4,7 +4,7 @@ import genDiff from '..';
 
 const gendiff = () => {
   program
-    .version('0.1.0')
+    .version('0.2.0')
     .arguments('<firstConfig> <secondConfig>')
     .description('Compares two configuration files and shows a difference.')
     .option('-f, --format [type]', 'Output format')
