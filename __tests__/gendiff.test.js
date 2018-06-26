7ff9055380170a674308f6eba14afafee8ddc599
Dmitriy Pavlenko <azeos1101@ya.ru>
2018-06-26 17:08:13 +0300

    :add tests for flat JSON diff

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,1 +1,11 @@
-test('empty test', () => expect(true).toBe(true));
+import fs from 'fs';
+import genDiff from '../src';
+
+test('difference beetwen two JSON files', () => {
+  const configPath1 = fs.realpathSync('__tests__/__fixtures__/before.json');
+  const configPath2 = fs.realpathSync('__tests__/__fixtures__/after.JSON');
+  const diff = fs.readFileSync('__tests__/__fixtures__/diff', 'utf-8');
+  const reverseDiff = fs.readFileSync('__tests__/__fixtures__/reverseDiff', 'utf-8');
+  expect(genDiff(configPath1, configPath2)).toBe(diff);
+  expect(genDiff(configPath2, configPath1)).toBe(reverseDiff);
+});
