3c84021b2f4de6b47081d42c678a9751c0e7889a
Dmitriy Pavlenko <azeos1101@ya.ru>
2018-06-29 13:10:40 +0300

    add tests for incorrect output format

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -31,7 +31,7 @@ test('difference beetwen two INI files', () => {
 test('difference beetwen two incorrect type files', () => {
   const configPath1 = fs.realpathSync('__tests__/__fixtures__/before.abc');
   const configPath2 = fs.realpathSync('__tests__/__fixtures__/after.INI');
-  const errorMessage = 'unkown format: .abc';
+  const errorMessage = 'unkown file format: .abc';
 
   function badFirstFileFormat() {
     genDiff(configPath1, configPath2);
@@ -59,4 +59,15 @@ test('JSON difference beetwen two YAML files', () => {
   const configPath2 = fs.realpathSync('__tests__/__fixtures__/after.YML');
   const diff = fs.readFileSync('__tests__/__fixtures__/jsonDiff', 'utf-8');
   expect(genDiff(configPath1, configPath2, 'json')).toBe(diff);
+});
+
+test('incorrect type difference beetwen two JSON files', () => {
+  const configPath1 = fs.realpathSync('__tests__/__fixtures__/before.json');
+  const configPath2 = fs.realpathSync('__tests__/__fixtures__/after.JSON');
+  const errorMessage = 'unkown format: abcdef';
+
+  function badFormatType() {
+    genDiff(configPath1, configPath2, 'abcdef');
+  }
+  expect(badFormatType).toThrowError(errorMessage);
 });
